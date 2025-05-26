4d0b4c4a6ea8cd3c78dafe3378549c331e85d518
Samuel <samuelfilipevianadois@gmail.com>
2025-05-26 17:40:51 -0300

    adiciona logica para historico de conversa

--- src/main/java/br/org/coletivoJava/integracoes/restIntollama/implementacao/IntegracaoRestIntollamaConversaSessaoAtual.java
+++ src/main/java/br/org/coletivoJava/integracoes/restIntollama/implementacao/IntegracaoRestIntollamaConversaSessaoAtual.java
@@ -1,16 +1,16 @@
 package br.org.coletivoJava.integracoes.restIntollama.implementacao;
 
+import br.org.coletivoJava.integracoes.ollama.api.util.UtilOllamaConversas;
 import br.org.coletivoJava.integracoes.restIntollama.api.InfoIntegracaoRestIntollamaChat;
 import br.org.coletivoJava.integracoes.ollama.api.chat.FabApiRestOllamaChat;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.implementacao.AcaoApiIntegracaoAbstrato;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.api.FabTipoAgenteClienteApi;
 import com.super_bits.modulosSB.SBCore.modulos.objetos.registro.Interfaces.basico.ItfUsuario;
-import jakarta.json.Json;
-import jakarta.json.JsonObject;
+import jakarta.json.*;
 
 @InfoIntegracaoRestIntollamaChat(tipo = FabApiRestOllamaChat.CONVERSA_SESSAO_ATUAL)
 public class IntegracaoRestIntollamaConversaSessaoAtual extends AcaoApiIntegracaoAbstrato {
-	private static final String NOME_MODELO = "tinyllama:1.1b";
+	private static final String NOME_MODELO = "CasanovaIA:latest";
 	public IntegracaoRestIntollamaConversaSessaoAtual(
 			final FabTipoAgenteClienteApi pTipoAgente,
 			final ItfUsuario pUsuario, final java.lang.Object... pParametro) {
@@ -20,15 +20,29 @@ public class IntegracaoRestIntollamaConversaSessaoAtual extends AcaoApiIntegraca
 
 	@Override
 	public String gerarCorpoRequisicao() {
-		String mensagem = (String) getParametros()[0];
-		Object[] parametrosMensagens = (Object[]) getParametros()[1];
-		//TODO: AJUSTAR OS PARAMETROS
-		String role = "";
-		JsonObject json = Json.createObjectBuilder()
-				.add("model", NOME_MODELO)
-				.add("messages", Json.createArrayBuilder().add(role))
+		String novaMensagemUsuario = (String) getParametros()[0];
+
+		//TODO: CHAMAR A CHAVE POR PARAMETROS E N HARDCODED(CONFERIR COMO MELHORAR ISSO)
+
+		JsonObject conversa = UtilOllamaConversas.lerConversa("teste123");
+		JsonArray mensagensAnteriores = conversa.getJsonArray("messages");
+
+		JsonArrayBuilder mensagensArrayBuilder = Json.createArrayBuilder();
+		for(JsonValue mensagem : mensagensAnteriores){
+			mensagensArrayBuilder.add(mensagem);
+		}
+		mensagensArrayBuilder.add(Json.createObjectBuilder()
+				.add("role", "user")
+				.add("content", novaMensagemUsuario));
+
+		JsonObject jsonPrincipal = Json.createObjectBuilder()
+				.add("model", conversa.getString("model"))
+				.add("messages", mensagensArrayBuilder.build())
 				.add("stream", false)
+				.add("keep_alive", "10m")
 				.build();
-		return json.toString();
+
+		return jsonPrincipal.toString();
+
 	}
 }
